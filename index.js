47da00ecc86306a921d52306425d7ddf51b3886a
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-03-31 22:29:23 +0300

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+// Create a variable in the global scope
+
+var customerName = 'bob';
+
+// function that accessed a global declared variable
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; 
+}
